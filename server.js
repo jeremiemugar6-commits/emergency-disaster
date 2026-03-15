d4968ad9c135f596fbab1abbea3343beef5f45bf
Jeremie Code <jeremiemugar6@gmail.com>
2026-03-15 14:30:48 +0800

    Delete server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.use(express.static("."));
-
-app.listen(process.env.PORT, () => {
-  console.log("Emergency Disaster System Running");
-});
