7055d078579572e68ed081c86a22ccac04a08d5f
Jeremie Code <jeremiemugar6@gmail.com>
2026-03-15 11:55:08 +0800

    Initialize Express server for Emergency Disaster System

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("."));
+
+app.listen(process.env.PORT, () => {
+  console.log("Emergency Disaster System Running");
+});
